49bff693cc13adb1c4345722b227764511b423a7
LokiLiang <56328415+Loki-Liang@users.noreply.github.com>
2026-09-27 00:19:45 +0800

    test(compat): 锁定 SessionManager 原公共构造签名

--- tests/OpenDeviceStudio.Tests/AcquisitionSessionTests.cs
+++ tests/OpenDeviceStudio.Tests/AcquisitionSessionTests.cs
@@ -746,6 +746,15 @@ public sealed class AcquisitionSessionTests
     }
 
     [Fact]
+    public void Session_manager_preserves_timeprovider_constructor_binary_surface()
+    {
+        var constructor = typeof(AcquisitionSessionManager).GetConstructor([typeof(TimeProvider)]);
+
+        Assert.NotNull(constructor);
+        using var manager = new AcquisitionSessionManager(TimeProvider.System);
+    }
+
+    [Fact]
     public async Task Host_owns_one_session_manager_and_stops_live_sessions()
     {
         var services = new ServiceCollection();
